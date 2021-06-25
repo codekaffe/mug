4f53f37d3891703f1cb25ecf6859824e536868b2
Joaquim Neto <joaquimmy@gmail.com>
2021-06-24 23:34:47 -0300

    fix report task

--- src/tasks/sendDailyCodingReport.task.js
+++ src/tasks/sendDailyCodingReport.task.js
@@ -8,7 +8,7 @@ const moment = require('moment');
 
 module.exports = new Task({
   name: 'Send Daily Coding Report',
-  time: '0 0 24 * * *',
+  time: '0 0 23 * * *',
   async run(bot, fireDate) {
     const channel = bot.guilds.cache
       .get('683463960501944327')
