c10d1fb0eb8da76634e645ec24823e476cc5de90
Joaquim Neto <joaquimmy@gmail.com>
2021-09-17 17:51:28 -0300

    feat(todos): send messages to todos by reacting

--- src/events/todos/reaction.event.js
+++ src/events/todos/reaction.event.js
@@ -3,9 +3,22 @@ const { EventHandler } = require('sensum');
 module.exports = new EventHandler({
   name: 'messageReactionAdd',
   run(bot, reaction) {
-    if (reaction.message.channel.id !== '888472757900288090') return;
-    if (['☑️', '✔️', '✅', '❤️'].includes(reaction.emoji.name)) {
-      reaction.message.delete().catch(console.log);
+    if (reaction.message.channel.id === '888472757900288090') {
+      if (['☑️', '✔️', '✅', '❤️'].includes(reaction.emoji.name)) {
+        reaction.message.delete().catch(console.log);
+      }
+    } else {
+      if (message.content.startsWith('todo:')) {
+        const msg = message.content.replace('todo:', '').trim();
+        const c = bot.guilds.cache
+          .get('683463960501944327')
+          .channels.cache.get('888472757900288090');
+        if (c instanceof TextChannel) {
+          c.send(`- ${msg}`)
+            .then(() => reaction.message.delete())
+            .catch(() => {});
+        }
+      }
     }
   },
 });
