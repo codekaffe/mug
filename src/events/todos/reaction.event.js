d07dcd7186179a1edb242c1b182c0d134572af92
Joaquim Neto <joaquimmy@gmail.com>
2021-11-18 14:14:29 -0300

    feat(todos): add :eyes: as a todo emote

--- src/events/todos/reaction.event.js
+++ src/events/todos/reaction.event.js
@@ -1,15 +1,17 @@
 const { TextChannel } = require('discord.js');
 const { EventHandler } = require('sensum');
 
+const listOfTodoEmotes = ['☑️', '✔️', '✅', '❤️', '👀'];
+
 module.exports = new EventHandler({
   name: 'messageReactionAdd',
   run(bot, reaction) {
     if (reaction.message.channel.id === '888472757900288090') {
-      if (['☑️', '✔️', '✅', '❤️'].includes(reaction.emoji.name)) {
+      if (listOfTodoEmotes.includes(reaction.emoji.name)) {
         reaction.message.delete().catch(console.log);
       }
     } else {
-      if (['☑️', '✔️', '✅', '❤️'].includes(reaction.emoji.name)) {
+      if (listOfTodoEmotes.includes(reaction.emoji.name)) {
         const msg = reaction.message.content.replace('todo:', '').trim();
         const c = bot.guilds.cache
           .get('683463960501944327')
